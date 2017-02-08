6446e8192832beea8fe138f96b56e801695ffb93
MalaCode <jonathanchee@sbcglobal.net>
2017-02-08 15:01:35 -0800

    created cmdline.hpp & created cmdline.cpp

---
+++ cmdline.cpp
@@ -0,0 +1,7 @@
+#include "cmdline.hpp"
+
+Cmdline::Cmdline(string cmd1, string cmd2 = "")
+{
+	 Lcmd = cmd1;
+	 Rcmd = cmd2;
+}
--- cmdline.hpp
+++ cmdline.hpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
+
+class Cmdline
+{
+	public:
+		Cmdline( string cmd1, string cmd2 = "" );
+		virtual void execute() = 0;
+	protected:
+		string Lcmd;
+		string Rcmd;
+};
