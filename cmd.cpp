5a2664866d81bfb7341f31f8b1a154b60a9b1280
awilk003 <awilk003@ucr.edu>
2017-02-14 15:55:57 -0800

    fork and exec test

--- cmd.cpp
+++ cmd.cpp
@@ -1,24 +1,58 @@
 #include "cmd.hpp"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 Cmd::Cmd(string input1)
 {
 	uCmd = input1;
-//	cout << "uCmd: " << uCmd << endl;
+	cout << "uCmd: " << uCmd << endl;
 }
 
 
-bool Cmd::isValid()
+/*bool Cmd::isValid()
 {
-//	cout << "Ucd: " << uCmd << endl;
+	cout << "Ucd: " << uCmd << endl;
 	int i = system(uCmd.c_str());
 	return i == 0;
 }
 
 bool Cmd::execute()
 {
-//	system(uCmd.c_str());	
+	system(uCmd.c_str());	
 	return isValid();
 	
+}*/
+
+void Cmd::execute(string cmd_s)
+{
+  
+
+  char* args[2];
+  args[0] = (char*)cmd_s.c_str();
+  args[1] = NULL;
+  
+   if ((pid = fork()) == -1) 
+    {
+        perror("fork"); 
+        return ;
+    }
+    if (pid == 0) // child process
+    {
+       if(execvp(args[0], args) == -1)
+       {
+	 perror("exec");
+       }
+    }
+    else // parent process
+    {
+      if(wait(0) == -1)
+      {
+	perror("wait");
+      }
+    }
+    return;
 }
 
 
