6e169880ef8193dd78affa82457535cf09c4ba41
MalaCode <jonathanchee@sbcglobal.net>
2017-02-14 11:58:44 -0800

    edit

--- cmd.cpp
+++ cmd.cpp
@@ -3,20 +3,20 @@
 Cmd::Cmd(string input1)
 {
 	uCmd = input1;
-	cout << "uCmd: " << uCmd << endl;
+//	cout << "uCmd: " << uCmd << endl;
 }
 
 
 bool Cmd::isValid()
 {
-	cout << "Ucd: " << uCmd << endl;
+//	cout << "Ucd: " << uCmd << endl;
 	int i = system(uCmd.c_str());
 	return i == 0;
 }
 
 bool Cmd::execute()
 {
-	system(uCmd.c_str());	
+//	system(uCmd.c_str());	
 	return isValid();
 	
 }
