9908bccc941e7f0f815d3b5f46359abb3c9e0ebb
MalaCode <jonathanchee@sbcglobal.net>
2017-02-22 22:48:50 -0800

    fixed last char deletion error

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 using namespace boost;
 
 
@@ -89,11 +90,14 @@ int main()
 		
 		    for (unsigned i = 0; i < cmds.size(); i += 2)
 		    {
-			    cmds.at(i).erase(cmds.at(i).end()-1);
+			    if (cmds.at(i).at(cmds.at(i).size()-1) == ' ')
+			    {
+			    	cmds.at(i).erase(cmds.at(i).end()-1);
+			    }
 		    }
 		    for (unsigned i = 2; i < cmds.size(); i += 2)
-		    {
-		    	    cmds.at(i).erase(cmds.at(i).begin()); 
+		    { 
+		   	    cmds.at(i).erase(cmds.at(i).begin()); 
 		    }
 
 /*
@@ -151,6 +155,8 @@ int main()
 			
 			    for (unsigned j = 1; j < substr.size(); j += 2)	//ITERATING THROUGH THE PARSER VECTOR
 			    {
+				if (isValid) { cout << "TRUE" << endl;}
+				else {cout << "FALSE" << endl;}
 				    Cmd* uCmd = new Cmd(cmds.at(j+1));		// CREATE NEW COMMANDS FOR EACH PARSER
 				    if (cmds.at(j) == ";")
 				    {
